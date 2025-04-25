3040dc36fbd5aba51383a944f8d2b4a816ddc03b
Christopher Douglass <christopher.douglass@mercedes-benz.com>
2025-04-25 10:01:16 -0700

    added intro to two pointer

--- 02_two_pointer/05_count_pairs_whose_sum_is_less_than_target.py
+++ 02_two_pointer/05_count_pairs_whose_sum_is_less_than_target.py
@@ -1,20 +1,18 @@
 import pytest
+"""
+Counts the number of pairs whose sum is less than a given target.
 
-def count_pairs_whose_sum_is_less_than_target():
-    """
-    Counts the number of pairs whose sum is less than a given target.
+Given an array of integers and a target value, the task is to count how many pairs of elements from the array
+have a sum less than the given target value.
 
-    Given an array of integers and a target value, the task is to count how many pairs of elements from the array
-    have a sum less than the given target value.
+Args:
+    nums (List[int]): A list of integers to find pairs from.
+    target (int): The target value to compare the sum of the pairs against.
 
-    Args:
-        nums (List[int]): A list of integers to find pairs from.
-        target (int): The target value to compare the sum of the pairs against.
-
-    Returns:
-        int: The number of pairs whose sum is less than the target.
-    """
-
+Returns:
+    int: The number of pairs whose sum is less than the target.
+"""
+def count_pairs_whose_sum_is_less_than_target(nums):
     pass
 
 def test_count_pairs_whose_sum_is_less_than_target():

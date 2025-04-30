132aeefb63c9b3467c5428a3f6b5d0cb4f284444
Christopher Douglass <christopher.douglass@mercedes-benz.com>
2025-04-30 09:50:02 -0700

    solved cyclic sort

--- 05_cyclic_sort/cyclic_sort.py
+++ 05_cyclic_sort/cyclic_sort.py
@@ -1,24 +1,40 @@
+
+"""
+Sorts an array containing `n` distinct numbers taken from the range [0, n] or [1, n].
+
+The algorithm places each number at its correct index by swapping elements in-place,
+resulting in an O(n) time complexity and O(1) space complexity.
+
+Args:
+    nums (List[int]): The unsorted list of integers.
+
+Returns:
+    None: The input list is modified in-place to be sorted.    
+"""
+
 import pytest
 
-def cyclic_sort():
-    """
-    Sorts an array containing `n` distinct numbers taken from the range [0, n] or [1, n].
+def cyclic_sort(nums: list[int]) -> list[int]:
+    for i in range(0, len(nums)): 
+        while i != nums[i]:
+            swap_indexes(nums, i, nums[i])
+    return nums
+    
 
-    The algorithm places each number at its correct index by swapping elements in-place,
-    resulting in an O(n) time complexity and O(1) space complexity.
+def swap_indexes(nums: list[int], i: int, j: int) -> list[int]:
+    print(f"nums: {nums}, i {i}, j {j}")
+    nums[i], nums[j] = nums[j], nums[i]
+    print(f"post-swap: nums: {nums}")
+    return nums
 
-    Args:
-        nums (List[int]): The unsorted list of integers.
-
-    Returns:
-        None: The input list is modified in-place to be sorted.
-    """
-
-    pass
-
-def test_cyclic_sort():
-    result = cyclic_sort()
-    assert result is None  # TODO: update assertion
+ 
+@pytest.mark.parametrize("nums, expectation",
+                         [
+                             ([1,3,5,2,4,0], [0,1,2,3,4,5]),
+                         ])
+def test_cyclic_sort(nums, expectation):
+    result = cyclic_sort(nums)
+    assert result == expectation
 
 if __name__ == "__main__":
     import sys
